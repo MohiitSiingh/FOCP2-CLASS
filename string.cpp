609f8f79790791c21402ea0da43c2ab5ae6eb938
Mohit <mohiitsiingh10@gmail.com>
2026-01-20 07:40:02 +0000

    Jan 20, 2026, 1:10 PM

--- string.cpp
+++ string.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 int main(){
  std::string name;
+ int n=9;
  std::cout<<"enter ";
-std::cin>>name; //it will take space as enter
- // std::cout<<"name is "<<name; 
- std::cin ignore();
+std::cin>>n; //it will take space as enter
+  std::cout<<"n is "<<name; 
+// std::cin ignore();
  getline(std::cin,name);
  std::cout<<"name is "<<name;
  
