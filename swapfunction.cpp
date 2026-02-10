356de22710ad6b8f46581c9ad53e81b501c92a52
Mohit <mohiitsiingh10@gmail.com>
2026-02-10 08:15:28 +0000

    Feb 10, 2026, 1:45 PM

--- swapfunction.cpp
+++ swapfunction.cpp
@@ -5,16 +5,18 @@ int swap(int a,int b);
 int main(){
     int  a = 23,b=9;
    // int *x=&a;
-    int &r = a;
+   // int &r = a;
   //  int c = swap(&a,&b);
 
-    cout<<a<<b<<"\n"<<swap(a,b);
-    cout<<"values after swap"<<a<<b;
+    cout<<a<<b<<"\n";
+    cout<<"v is "<<swap(a,b);
+    cout<<"values after swap"<<a<<"b is:"<<b;
     return 0;
 }
 int swap(int a,int b){
     a=a+b;
     b=a-b;
     a=a-b;
-    cout<<"values of a and b after swap: "<<a<<"\n"<<b;
+    return a,b;
+  //  cout<<"values of a and b after swap: "<<a<<"\n"<<b;
 }
